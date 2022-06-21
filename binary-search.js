e8bc173cfeb673745443c6e124fd2e7bd96884fc
daveh <dave.halladay@outlook.com>
2022-06-21 13:40:45 -0600

    completed iterativeSearch function

--- binary-search.js
+++ binary-search.js
@@ -7,7 +7,7 @@
 // condition, then the middle number now becomes the end number and the process starts again. If it fits the second, then
 // the middle number becomes the start number and the process goes again until you reach your number. 
 
-// recursive 
+// **recursive **
 
 // in the recursive version, you need start and end provided.
 
@@ -17,30 +17,50 @@ function recursiveSearch(arr, x, start, end) {
   if (start > end) return false;
 
   // you're looking at the index, so you need to fin the middle index
-  let mid = Math.floor((start + end)/2);
+  let mid = Math.floor((start + end) / 2);
 
-  // compare the value of the middle to x (what you're searching for)
-  if (arr[mid]===x) return true;
+  // compare the value of the middle to x (what you're searching for). If it equals the midpoint, return true
+  if (arr[mid] === x) return true;
 
   // check to see if x is greater than or less than the middle index value, 
   // then based on that call the recurvsive search again 
 
-  if(arr[mid] > x) 
-    return recursiveSearch(arr, x, start, mid-1);
+  if (arr[mid] > x)
+    return recursiveSearch(arr, x, start, mid - 1);
 
-  else 
-    return recursiveSearch(arr, x, mid+1, end)
+  else
+    return recursiveSearch(arr, x, mid + 1, end)
 }
 
 let array = [1, 3, 5, 7, 8, 9, 10, 11]
-recursiveSearch(array, 6, 0, array.length-1);
+recursiveSearch(array, 6, 0, array.length - 1);
 
+
+// **iterative**
+
+// in the iterative approach, you do not need the start and end provided. 
 
 function iterativeSearch(arr, x) {
-  let start=0, end=arr.length-1;
-  let mid = Math.floor((start + end)/2)
 
-  console.log(start, end, mid, arr.length, arr.length-1)
+  // Start will always be 0, end will always be length-1 to start off with
+  let start = 0, end = arr.length - 1;
+
+  // set condition so that this will loop as long as the start is less than or equal to the end 
+  // (because we keep redeclaring the start and end variables it will eventually meet this criteria)
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    if (arr[mid] === x) return true;
+
+    // if x is greater than the mid point found, declare start variable to be mid +1
+    else if (arr[mid] < x)
+      start = mid + 1;
+    else
+      end = mid - 1;
+  }
+
+  return false;
 
 }
 
+let itArray = [1, 3, 5, 7, 8, 9, 10, 11]
+iterativeSearch(itArray, 6);

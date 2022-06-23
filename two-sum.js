f58111c2333da122ed0acd9477be735e38806ccc
daveh <dave.halladay@outlook.com>
2022-06-23 13:45:35 -0600

    added hashmap solution for twoSum algorithm with descriptions

--- two-sum.js
+++ two-sum.js
@@ -4,7 +4,11 @@
 
 // You can return the answer in any order.
 
-// first attempt - nested loops - result = time limit exceeded. 
+// @param {number[]} nums
+// @param {number} target
+// @return {number[]}
+
+// first attempt - nested loops - result = provided correct results, but time limit exceeded. 
 
 // var twoSum = function (nums, target) {
 //   for (var i = 0; i < nums.length; i++) {
@@ -18,6 +22,34 @@
 //   }
 // }
 
+// hash-mapping
+
+// for the has mapping method, you will create an empty object and store the key value pairs from the nums array in your obj
+// the first time you reach a correct index for a value that adds up to the target, it's corresponding value/index pair
+// will not be in the map so instead that value/index key value pair will be added to the map for later use. Then, when your
+// function reaches the second key value pair, it will find the first value and will be able to determine that the two
+// values add up to the target number and both will be included in the output. 
+
+var twoSumMap = function(nums, target) {
+  // create empty object, requires key value pair of value : index
+  let map = {};
+
+  for(let i = 0; i < nums.length; i++) {
+    // n is the value of nums at index i
+    const n = nums[i];
+
+    // this will check the map object. Because we add the value as the key and the index as the value in the map object,
+    // this is looking up to see if the value of target-n exists in the map. This will only be the case when we reach
+    // the second value that adds up to the target.
+    if(map[target - n] !== undefined) {
+      // once we find both values, return the index value from map object as the first index and i as the second.
+      return [map[target-n], i];
+    }
+    // if the combined values are both not present, add the key value pair (n is value, i is index) to the map object
+    map[n]=i
+  }
+
+}
 
 
 let nums = [2,7,11,15], target = 9;

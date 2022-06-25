e2bb30ed7b28b91bd2ee26472be84cc1b347dd9e
daveh <dave.halladay@outlook.com>
2022-06-24 22:07:25 -0600

    add inSubsequence and Isomorphic Strings, added solution for isSubsequence

---
+++ is-subsequence.js
@@ -0,0 +1,21 @@
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+var isSubsequence = function(s, t) {
+
+  let i = 0, j = 0;
+// basically two loops. the first will go through var s. If a string is a subsequence, then i will increase faster than j, however, if j becomes equal to t.length first, then it not subsequence. 
+  while(i < s.length && j < t.length) {
+    if(s[i] === t[j]) {
+      i++
+    }
+    j++
+  }
+  
+  return i === s.length ? true: false;
+    
+};
+
+
+isSubsequence(s, t);

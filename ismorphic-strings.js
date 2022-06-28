74692425a0097ca3bec5f11514aa513c9342c0d1
daveh <dave.halladay@outlook.com>
2022-06-28 12:18:43 -0600

    added Set to isIso and additional logic to add unique t character values

--- ismorphic-strings.js
+++ ismorphic-strings.js
@@ -11,17 +11,39 @@
 // assign s[0]:t[0] to the hash map. on the next loop check the map, if the letter in s exists (key) compare the value of t(value). if they are the same move to the next
 // if it's not there add it.
 
-var isIsomorphic = function(s, t) {
-    const mapST = new Map();
+// var isIsomorphic = function (s, t) {
+//   const mapST = new Map();
 
-    for (var i = 0; i <= s.length -1; i++) {
+//   for (var i = 0; i <= s.length - 1; i++) {
 
-      if (mapST.has(s[i]) && mapST.get(s[i]) != t[i]) {
-        return false
-      }
-      mapST.set(s[i], t[i])
+//     if (mapST.has(s[i]) && mapST.get(s[i]) != t[i] || mapST.values(t[i])) {
+//       return false
+//     }
+//     mapST.set(s[i], t[i])
+//   }
+//   return true
+// };
+
+// isIsomorphic("badc", "baba")
+
+// the above failed on badc baba because it does not force unique values of key indicators(s). I believe I need to add Set somehow.
+
+var isIsomorphic = function (s, t) {
+  const mapST = new Map();
+  const setST = new Set();
+
+  for (var i = 0; i <= s.length - 1; i++) {
+
+    if (mapST.has(s[i]) && mapST.get(s[i]) != t[i] ) {
+      return false
+    } else if(setST.has(t[i])) {
+      return false
     }
- return true
+    mapST.set(s[i], t[i]);
+    setST.add(t[i]);
+  }
+  return true
 };
 
-isIsomorphic("foo", "bar")
+isIsomorphic("badc", "baba")
+
